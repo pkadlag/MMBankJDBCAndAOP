ca12544ce230ecf11706e6965271b8e0126a0476
Kadlag <punam.kadlag@capgemini.com>
2019-01-10 09:44:28 +0530

    Add JDBC and AOP files of MMBankAccount

---
+++ src/main/java/com/cg/app/account/dao/SavingsAccountDAO.java
@@ -0,0 +1,34 @@
+package com.cg.app.account.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import com.cg.app.account.*;
+import com.cg.app.exception.AccountNotFoundException;
+
+public interface SavingsAccountDAO {
+
+	SavingsAccount createNewAccount(SavingsAccount account);
+
+	int updateAccount(int accountNumber, String accountHolderName);
+
+	SavingsAccount getAccountById(int accountNumber) throws AccountNotFoundException;
+
+	SavingsAccount deleteAccount(int accountNumber) throws AccountNotFoundException;
+
+	SavingsAccount getAccountByName(String accountHolderName) throws AccountNotFoundException;
+
+	List<SavingsAccount> getAccountByBalance(double minmumBalance, double maximumBalance)
+			throws AccountNotFoundException;
+
+	double checkAccountBalance(int accountNumber) throws AccountNotFoundException;
+
+	List<SavingsAccount> getAllSavingsAccount() ;
+
+	void updateBalance(int accountNumber, double currentBalance);
+
+	List<SavingsAccount> sortAllSavingsAccount(int choice);
+
+	/* void commit() ; */
+
+}
